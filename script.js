16cf829cae188d1934347f550c31211a86766a0c
whatisartcomic <southernadd+whatisart@gmail.com>
2025-05-29 22:05:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,50 +9,60 @@ $(document).ready(function() {
     .then(data => {
       const $fb = $('#flipbook').empty();
 
-      // Title page
-      $fb.append(`
-        <div class="page title">
-          <h1>WHAT IS ART?</h1>
-        </div>
-      `);
+      // Generate a random order for the comic pages
+      const pagesOrder = data.map((_, i) => i);
+      // Fisher-Yates shuffle
+      for (let i = pagesOrder.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [pagesOrder[i], pagesOrder[j]] = [pagesOrder[j], pagesOrder[i]];
+      }
 
-      // Comic pages (no internal haiku)
-      data.forEach((_, i) => {
-        const num = i + 1;
-        $fb.append(`
-          <div class="page">
-            <div class="comic-frame">
-              <img src="images/webp/${num}.webp" alt="Comic ${num}">
-            </div>
-          </div>
-        `);
+      // Title page
+      $fb.append(
+        `<div class="page title">
+           <h1>WHAT IS ART?</h1>
+         </div>`
+      );
+
+      // Comic pages in randomized order (haikus match their image index)
+      pagesOrder.forEach(idx => {
+        const num = idx + 1;
+        $fb.append(
+          `<div class="page">
+             <div class="comic-frame">
+               <img src="images/webp/${num}.webp" alt="Comic ${num}">
+             </div>
+           </div>`
+        );
       });
 
       // Initialize Turn.js with dynamic sizing
       const $firstImg = $fb.find('img').first();
       const initFlipbook = (w, h) => {
-        $fb
-          .css({ width: w + 'px', height: h + 'px' })
-          .turn({
-            width: w,
-            height: h,
-            display: 'single',
-            autoCenter: false,
-            gradients: true,
-            acceleration: false,
-            duration: 900,
-            cornerSize: 100 // enable mouse corner drag area
-          });
+        $fb.css({ width: w + 'px', height: h + 'px' }).turn({
+          width: w,
+          height: h,
+          display: 'single',
+          autoCenter: false,
+          gradients: true,
+          acceleration: true,
+          duration: 900,
+          cornerSize: 100
+        });
 
-        // Update haiku text on each turn
+        // Update haiku text on each turn, using the randomized order
         $fb.off('turned').on('turned', function(e, page) {
-          $('#haikuDisplay').text(page === 1 ? '' : data[page - 2]);
+          if (page === 1) {
+            $('#haikuDisplay').text('');
+          } else {
+            const dataIdx = pagesOrder[page - 2];
+            $('#haikuDisplay').text(data[dataIdx]);
+          }
         });
 
         // Random comic on "?" button click
         $('#nextBtn').off('click').on('click', function() {
           const totalPages = $fb.turn('pages');
-          // Pages 2 through totalPages are comics
           const randomPage = Math.floor(Math.random() * (totalPages - 1)) + 2;
           $fb.turn('page', randomPage);
         });
