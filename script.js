65c090f213a3f56b3b634126308af906d274f50e
whatisartcomic <southernadd+whatisart@gmail.com>
2025-05-31 13:08:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -159,8 +159,10 @@ $(document).ready(function() {
             p.textContent = '';
             buttonClickCount++;
 
-              document.getElementById('flipbook').style.borderLeft = `2px solid black`;
-
+              document.getElementById('flipbook').style.borderLeft = `2px solid #f2f2f2`;
+              const flipbook = document.getElementById('flipbook');
+              flipbook.style.setProperty('--border-width', '2px');
+              flipbook.style.setProperty('--border-color', '#f2f2f2');
             const thickness = Math.ceil(buttonClickCount / 4);
               document.querySelector('#flipbook::before')?.style?.setProperty?.('--border-width', `${thickness}px`);
 
